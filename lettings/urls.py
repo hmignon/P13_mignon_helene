973ca40e243d639fd729f2704b915dcdf46cbdb0
hmignon <84913892+hmignon@users.noreply.github.com>
2022-04-01 15:34:02 +0200

    move templates to apps
    added url namespaces

--- lettings/urls.py
+++ lettings/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = 'lettings'
 urlpatterns = [
-    path('', views.lettings_index, name='lettings_index'),
+    path('', views.lettings_index, name='index'),
     path('<int:letting_id>/', views.letting, name='letting'),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -11,7 +11,7 @@ def profiles_index(request):
     """
     profiles_list = Profile.objects.all()
     context = {'profiles_list': profiles_list}
-    return render(request, 'profiles_index.html', context)
+    return render(request, 'profiles/index.html', context)
 
 
 def profile(request, username):
@@ -23,4 +23,4 @@ def profile(request, username):
     """
     profile = Profile.objects.get(user__username=username)
     context = {'profile': profile}
-    return render(request, 'profile.html', context)
+    return render(request, 'profiles/profile.html', context)
