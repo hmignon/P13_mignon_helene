6f6158fc9e6af09a8e0e7717142d864ac1499938
hmignon <84913892+hmignon@users.noreply.github.com>
2022-04-01 13:25:57 +0200

    pep8
    change comments to docstrings
    reorganize imports

--- oc_lettings_site/admin.py
+++ oc_lettings_site/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 
-from .models import Letting
 from .models import Address
+from .models import Letting
 from .models import Profile
-
 
 admin.site.register(Letting)
 admin.site.register(Address)
