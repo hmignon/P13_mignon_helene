0cb637f472a57c5ac35a0f3c9622201ec97d574d
hmignon <84913892+hmignon@users.noreply.github.com>
2022-04-01 16:10:03 +0200

    add tests
    rename index views
    update urls

--- lettings/views.py
+++ lettings/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from .models import Letting
 
 
-def lettings_index(request):
+def index(request):
     """
     Aenean leo magna, vestibulum et tincidunt fermentum, consectetur quis velit. Sed non
     placerat massa. Integer est nunc, pulvinar a tempor et, bibendum id arcu. Vestibulum ante
