724d0c1b4b230f34010138551a8a074423de4f0f
hmignon <84913892+hmignon@users.noreply.github.com>
2022-04-15 04:19:41 +0200

    Update settings.py

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+WHITENOISE_MANIFEST_STRICT = False
 
 # Sentry configuration
 sentry_sdk.init(
