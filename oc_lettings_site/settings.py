c4d059102782863fee043b674869630e7291510e
hmignon <84913892+hmignon@users.noreply.github.com>
2022-04-15 02:15:45 +0200

    update database settings

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -20,7 +20,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = env('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = int(env('DEBUG', default=0))
+DEBUG = bool(env('DEBUG', default=0))
 
 ALLOWED_HOSTS = ['*']
 
@@ -72,15 +72,19 @@ WSGI_APPLICATION = 'oc_lettings_site.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'oc-lettings-site.sqlite3'),
+if DEBUG is True:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'oc-lettings-site.sqlite3'),
+        }
     }
-}
-
-if DEBUG is False:
-    DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+else:
+    DATABASES = {
+        'default': {
+            dj_database_url.config(conn_max_age=600, ssl_require=True)
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
