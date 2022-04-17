f96fa2f68ed68a68b49db0b44a71a7b540d647ce
hmignon <84913892+hmignon@users.noreply.github.com>
2022-04-18 01:36:47 +0200

    update readme add setup_env.py for .env template creation add comment to config.yml

---
+++ setup_env.py
@@ -0,0 +1,11 @@
+""" Create .env file template for OC_lettings_site with generated random secret key"""
+
+from django.core.management.utils import get_random_secret_key
+
+with open(".env", "w") as f:
+    f.write(f"DJANGO_SECRET_KEY={get_random_secret_key()}\n")
+    f.write("SENTRY_DSN= \n")
+    f.write("DEBUG=\n")
+    f.close()
+
+print("\n.env file template created!")
